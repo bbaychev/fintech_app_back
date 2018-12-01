f15b263746af22286d971ce30eb9b309e73c4e11
Elle <aleksandrova.elena@outlook.com>
2018-12-01 12:30:05 +0200

    Heroku setup

--- script.js
+++ script.js
@@ -3,29 +3,37 @@ var mysql = require('mysql');
 var app = express();
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'fintech_db'
+
+    // mysql://b69ec27c5c0b5b:ef83cc53@eu-cdbr-west-02.cleardb.net/heroku_7ba11b197eba2eb?reconnect=true
+
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'b69ec27c5c0b5b',
+    password: 'ef83cc53',
+    database: 'heroku_7ba11b197eba2eb'
+
 });
 
 connection.connect(function (error) {
     if (!!error) {
         console.log('Error');
+        console.log(error.code);
     } else {
         console.log('Connected');
     }
+}
+);
+
+app.route('/e').get( (req, res) => {
+        connection.query("SELECT * FROM test", function(error, rows, fields){
+            if(!!error){
+                console.log('Error - query');
+            } else {
+                console.log("Success");
+                console.log(rows);
+            }
+        });
 });
 
-app.get('/e', function (req, res){
-    connection.query("SELECT * FROM table00", function(error, rows, fields){
-        if(!!error){
-            console.log('Error - query');
-        } else {
-            console.log("Success");
-            console.log(rows);
-        }
-    });
-});
 
-app.listen(8087);
+
+app.listen(3006);
